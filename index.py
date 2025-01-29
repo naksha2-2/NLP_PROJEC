6ef820c52358825646db67ad8b1d5e9928e8d20f
naksha <dominikafirebolt2000@gmail.com>
2025-01-29 07:51:46 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,4 @@
-import warnings
-warnings.filterwarnings('ignore')
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-import matplotlib.pyplot as plt
-from wordcloud import WordCloud
+
 
 
 
